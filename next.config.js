63c0ccedd872d3c56bf87690c6abd8904cbffc15
Nikita Kov <Kov_nikita2003@mail.ru>
2025-02-09 18:14:37 +0100

    deploing

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+	basePath: '/Portfolio_NK',
+	output: "export",  // <=== enables static exports
+	reactStrictMode: true,
+};
+
+module.exports = nextConfig;
